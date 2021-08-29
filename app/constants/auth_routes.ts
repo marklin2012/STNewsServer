7db0b1e478fe130b46cb3bd6abd1933ab0378c28
Mark Lin <marklin@Lenny.local>
2021-08-29 09:20:00 +0800

    feat: 路由白名单处理

---
+++ app/constants/auth_routes.ts
@@ -0,0 +1,15 @@
+export default [
+  // swagger 相关白名单
+  '/swagger-ui.html',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/swagger-ui-standalone-preset.js',
+  '/swagger-ui.css',
+  '/swagger-ui-bundle.js',
+  '/swagger-doc',
+  // 路由相关白名单
+  '/login/password',
+  '/login/pin',
+  '/login/checkcode',
+  '/user/password',
+]
