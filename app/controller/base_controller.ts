b858e4acf2ae1216ecc5530a4452af7cd165c14d
Mark Lin <marklin@Lenny.local>
2021-08-29 09:21:28 +0800

    feat: 成功方法封装添加 message 参数

--- app/controller/base_controller.ts
+++ app/controller/base_controller.ts
@@ -1,10 +1,10 @@
 import { Controller } from 'egg'
 
 export default class BaseController extends Controller {
-  success(data) {
+  success(data, message?) {
     this.ctx.body = {
       statusCode: 200,
-      message: 'success',
+      message: message ?? 'success',
       data,
     }
   }
