d2835463ed329062665e717087b1786733db662f
Mark Lin <marklin@Lenny.local>
2021-09-04 03:07:08 +0800

    feat: 收藏文章接口

--- app/controller/post.ts
+++ app/controller/post.ts
@@ -1,4 +1,6 @@
+import * as Boom from '@hapi/boom'
 import Post from '../model/post'
+import PostFavourite from '../model/post_favourite'
 import BaseController from './base_controller'
 
 /**
@@ -17,5 +19,38 @@ export default class PostController extends BaseController {
     const { per_page, skip } = ctx.state
     const result = await Post.find().limit(per_page).skip(skip).lean()
     this.success(result)
+  }
+  /**
+   * @summary 收藏文章
+   * @description
+   * @router put /post/favourite
+   * @response 200 responseBody 返回值
+   */
+  public async favouritePost() {
+    const { ctx } = this
+    ctx.validate({
+      post: { type: 'id', required: true },
+      status: { type: 'bool', required: false, default: true },
+    })
+    const { post, status } = ctx.request.body
+    const { id } = ctx.state.user
+    try {
+      await PostFavourite.findOneAndUpdate(
+        { post, user: id },
+        {
+          $setOnInsert: {
+            post,
+            user: id,
+          },
+          $set: {
+            status,
+          },
+        }
+      )
+      const message = status ? '已收藏该文章' : '已取消收藏该文章'
+      this.success('操作成功', message)
+    } catch (err) {
+      throw Boom.badData('用户或文章可能不存在，请稍后再试')
+    }
   }
 }
--- app/router.ts
+++ app/router.ts
@@ -4,7 +4,7 @@ export default (app: Application) => {
   const { controller, router } = app
   router.get('/', controller.home.index)
   router.get('/test/add_user', controller.test.addUserTest)
-  router.get('/post/list', controller.post.list)
+
   // 登录相关
   router.post('/login/password', controller.user.loginWithPassword)
   router.post('/login/pin', controller.user.loginWithPin)
@@ -16,4 +16,7 @@ export default (app: Application) => {
   router.get('/user/info', controller.user.getUserInfo)
   router.put('/user/favourite', controller.user.favouriteUser)
   router.get('/user/favourite/list', controller.user.geFavouriteUsers)
+  // 文章相关
+  router.get('/post/list', controller.post.list)
+  router.put('/post/favourite', controller.post.favouritePost)
 }
