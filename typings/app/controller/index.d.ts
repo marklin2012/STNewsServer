b0cbd33a67ff3e8dfe1321f2f63c12dee7777f98
Mark Lin <marklin@Lenny.local>
2021-11-03 19:38:41 +0800

    chore: 添加圈子相关 typeScript

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -4,9 +4,11 @@
 import 'egg';
 import ExportBaseController from '../../../app/controller/base_controller';
 import ExportComment from '../../../app/controller/comment';
+import ExportCommentMoment from '../../../app/controller/comment_moment';
 import ExportFeedback from '../../../app/controller/feedback';
 import ExportFile from '../../../app/controller/file';
 import ExportHome from '../../../app/controller/home';
+import ExportMoment from '../../../app/controller/moment';
 import ExportNotification from '../../../app/controller/notification';
 import ExportPost from '../../../app/controller/post';
 import ExportSearch from '../../../app/controller/search';
@@ -17,9 +19,11 @@ declare module 'egg' {
   interface IController {
     baseController: ExportBaseController;
     comment: ExportComment;
+    commentMoment: ExportCommentMoment;
     feedback: ExportFeedback;
     file: ExportFile;
     home: ExportHome;
+    moment: ExportMoment;
     notification: ExportNotification;
     post: ExportPost;
     search: ExportSearch;
